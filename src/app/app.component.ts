1ea0596a79f5cf285748327ed6d90b7757d65390
Krishna <kri.vri1989@gmail.com>
2022-05-30 14:27:49 +0530

    Implement SSR.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private metaTagService: Meta) {}
+
+  ngOnInit() {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Digamber Singh' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private titleService: Title, private metaService: Meta) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    this.titleService.setTitle('Devdactic SSR');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'The Devdactic SSR Page',
+    });
+    // Twitter
+    this.metaService.updateTag({
+      property: 'twitter:card',
+      content: 'summary_large_image',
+    });
+    this.metaService.updateTag({
+      property: 'twitter:title',
+      content: 'NEW ARTICLE OUT NOW',
+    });
+    this.metaService.updateTag({
+      property: 'twitter:description',
+      content: 'Check out this cool article',
+    });
+    this.metaService.updateTag({
+      property: 'twitter:image',
+      content:
+        'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1',
+    });
+    // Facebook
+    this.metaService.updateTag({ property: 'og:url', content: '/second' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({
+      property: 'og:description',
+      content: 'My Ionic SSR Page',
+    });
+    this.metaService.updateTag({
+      property: 'og:title',
+      content: 'My SSR Title!',
+    });
+    this.metaService.updateTag({
+      property: 'og:image',
+      content:
+        'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1',
+    });
+  }
 }
